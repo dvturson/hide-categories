640223755f5fc37d7ac39bf5ee23494d1406d7fc
Devran Turson <dvturson@gmail.com>
2026-05-29 11:22:41 -0400

    Initial files for MakeCode project

---
+++ custom.ts
@@ -0,0 +1,39 @@
+//% deprecated=true
+namespace music {
+}
+
+//% deprecated=true
+namespace led {
+}
+
+//% deprecated=true
+namespace game {
+}
+
+//% deprecated=true
+namespace images {
+}
+
+//% deprecated=true
+namespace pins {
+}
+
+//% deprecated=true
+namespace serial {
+}
+
+//% deprecated=true
+namespace control {
+}
+
+//% deprecated=true
+namespace radio {
+}
+
+//% deprecated=true
+namespace input {
+}
+
+//% deprecated=true
+namespace basic {
+}
